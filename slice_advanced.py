9388f7efcc80f1959a8890eae5b1920acaa7d1ee
Salvador Sanchez Pujol <ssanchezpujol@mail.austral.edu.ar>
2025-04-14 18:59:29 -0300

    Add files via upload

---
+++ slice_advanced.py
@@ -0,0 +1,4 @@
+ def slice_avanzado():
+	texto = input("Ingrese un texto: ")
+	resultado = texto[4::2]
+	print(resultado)
---
+++ slice_simple.py
@@ -0,0 +1,8 @@
+def slice_simple():
+	texto = "Awesome"
+	Awe = (texto[:3].lower())
+	Eso = (texto[2:5].lower())
+	Full = (texto[:4].lower()+texto[-3:])
+	print(Awe)
+	print(Eso)
+	print(Full)
