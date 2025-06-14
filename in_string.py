b032693b3f3b7301fc0dc5a6f89b8358448ecd39
Salvador Sanchez Pujol <ssanchezpujol@mail.austral.edu.ar>
2025-06-14 20:49:09 -0300

    in_string.py

--- in_string.py
+++ in_string.py
@@ -1,7 +1,7 @@
 def check_vowels():
-    texto = input().lower()
-    print(f"Contiene a: {'a' in texto}")
-    print(f"Contiene e: {'e' in texto}")
-    print(f"Contiene i: {'i' in texto}")
-    print(f"Contiene o: {'o' in texto}")
-    print(f"Contiene u: {'u' in texto}")
+    nombre = input().lower()
+    print("Contiene a:", 'a' in nombre)
+    print("Contiene e:", 'e' in nombre)
+    print("Contiene i:", 'i' in nombre)
+    print("Contiene o:", 'o' in nombre)
+    print("Contiene u:", 'u' in nombre)
