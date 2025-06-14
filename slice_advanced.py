b53e4dbc99ff8c1965ede0dae9dbda28856fb8b8
Salvador Sanchez Pujol <ssanchezpujol@mail.austral.edu.ar>
2025-06-14 20:42:47 -0300

    slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,6 +1,6 @@
-def slice_texto():
-    texto = "Awesome".lower()  # Nos aseguramos de que esté en minúsculas
+def slice_text():
+    texto = "Awesome".lower()
     mitad = len(texto) // 2
-    print(texto[:3])                 # Primeras 3 letras
-    print(texto[mitad - 1:mitad + 2])  # 3 letras del medio
-    print(texto[:4] + texto[-3:])   # Primera a cuarta + antepenúltima a última
+    print(texto[:3])  # primeras 3
+    print(texto[mitad - 1:mitad + 2])  # 3 del medio
+    print(texto[:4] + texto[-3:])  # 0–4 y últimas 3
