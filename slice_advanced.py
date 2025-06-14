762a8b2d03d9cbe83df9af9b3f05dee3385ae0f7
Salvador Sanchez Pujol <ssanchezpujol@mail.austral.edu.ar>
2025-06-14 20:40:06 -0300

    slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,4 +1,6 @@
- def slice_avanzado():
-	texto = input("Ingrese un texto: ")
-	resultado = texto[4::2]
-	print(resultado)
+def slice_texto():
+    texto = "Awesome".lower()  # Nos aseguramos de que esté en minúsculas
+    mitad = len(texto) // 2
+    print(texto[:3])                 # Primeras 3 letras
+    print(texto[mitad - 1:mitad + 2])  # 3 letras del medio
+    print(texto[:4] + texto[-3:])   # Primera a cuarta + antepenúltima a última
